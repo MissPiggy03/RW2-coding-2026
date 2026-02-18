ca21079aba90796bf2b7df449bc72c540bd89070
MissPiggy03 <gterrell809@gmail.com>
2026-02-18 07:07:04 +0000

    created helloworld script

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+# the hash is a one line comment
+
+"""
+the triple quotes are a multi-line comment
+see
+believe me
+anything inside will not excute
+"""
+
+print("Hello world!")
+
+# this will print to the screen
+print("Hello world!")
+
