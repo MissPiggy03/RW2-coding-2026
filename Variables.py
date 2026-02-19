0a58caa2d4e0b1dbe31634dfba949877fc5a105b
MissPiggy03 <gterrell809@gmail.com>
2026-02-19 03:49:35 +0000

    Final changes for wed

--- Variables.py
+++ Variables.py
@@ -11,8 +11,15 @@ x = str(3)      # x will be '3'
 y = int(3)      # y will be 3
 z = float(3)    # z will be 3.0
 
+
 x = "John"
 print(x)
 #double quotes are the same as single quotes:
 x = 'John'
 print(x)
+
+thislist = ["subaru", "crosstrack", "GMC Truck", "Escalator"]
+for i in range(len(thislist))
+print(thislist[i])
+i = i + 1
+
