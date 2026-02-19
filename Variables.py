b70b098b17b8873e63b682072b3258bb5450c050
MissPiggy03 <gterrell809@gmail.com>
2026-02-19 02:55:14 +0000

    More Comments

--- Variables.py
+++ Variables.py
@@ -7,3 +7,12 @@ x = 4       # x is of type int
 x = "DeLen" # x is now of type str
 
 
+x = str(3)      # x will be '3'
+y = int(3)      # y will be 3
+z = float(3)    # z will be 3.0
+
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
