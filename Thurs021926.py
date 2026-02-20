1b83dfc37f9805d4e32fdb8d42ea88e7c619dfad
MissPiggy03 <gterrell809@gmail.com>
2026-02-20 03:07:13 +0000

    Thurs3

--- Thurs021926.py
+++ Thurs021926.py
@@ -1,7 +1,9 @@
 # this is a dictionary exercise
 teacher = {"name":"mike", "age","48"}
+
 # add a new dictionary element
 called city with a value of boston
+
 #print a sentence to the screen using f-strings that says
 # mike is 48 and lives in boston
 print(f"{teacher['name']} is {teacher['age']} and lives in {teacher['city']}"
